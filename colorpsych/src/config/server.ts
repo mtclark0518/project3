107f0ca34ed2c8d64ce4f918fc927c5a59849075
mtclark0518 <mtclark0518@gmail.com>
2017-08-24 09:40:29 -0600

    starts to add angular universal

--- colorpsych/src/config/server.ts
+++ colorpsych/src/config/server.ts
@@ -1,3 +1,7 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+import { platformServer, renderModuleFactory } from '@angular/platform-server';
+import { enableProdMode } from '@angular/core';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as flash from 'connect-flash';
@@ -7,12 +11,8 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import 'zone.js/dist/zone-node';
-import { platformServer, renderModuleFactory } from '@angular/platform-server';
-import { enableProdMode } from '@angular/core';
-// import { AppServerModuleNgFactory } from '../dist/ngfactory/src/app/app-server-module.ngfactory';
 
-import { router as expressRouter } from './routes'; 
+import { router as expressRouter } from './routes';
 
 const PORT = 3000;
 
@@ -24,22 +24,22 @@ app.use(bodyParser.json());
 
 app.use(expressRouter);
 
-let template = readFileSync(join(__dirname, '..', 'index.html')).toString();
+const template = readFileSync(join(__dirname, '..', 'index.html')).toString();
 
 app.engine('html', (_, options, callback) => {
     const opts = { document: template, url: options.req.url };
 
-    // renderModuleFactory(AppServerModuleNgFactory, opts)
-    // .then(html => callback(null, html));
+    renderModuleFactory(AppServerModuleNgFactory, opts)
+    .then(html => callback(null, html));
 });
 
 app.set('view engine', 'html');
 app.set('views', 'src');
 
-// app.get('*.*', express.static(join(__dirname, '..', 'dist')));
-// app.get('*', (req, res) => {
-//     res.render('index', { req });
-// });
+app.get('*.*', express.static(join(__dirname, '..', 'dist')));
+app.get('*', (req, res) => {
+    res.render('index', { req });
+});
 
 
 app.use(session({ secret: 'PROJECT-3'}));
@@ -47,7 +47,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// import './passport';(passport);
 
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
