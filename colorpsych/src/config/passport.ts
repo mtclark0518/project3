596ac0a91526771543f593069a524d98bc00d5f6
mtclark0518 <mtclark0518@gmail.com>
2017-08-24 19:37:57 -0600

    comments out passport.ts for the time being

--- colorpsych/src/config/passport.ts
+++ colorpsych/src/config/passport.ts
@@ -1,25 +1,25 @@
 /// reference types="passport" />
 
-import passport = require('passport');
-import express = require('express');
+// import passport = require('passport');
+// import express = require('express');
 
 
-interface IStrategyOptionsWithRequest {
-    usernameField: string;
-    passwordField: string;
-    passReqToCallback: true;
-}
-interface IVerifyOptions {
-    message: string;
-}
+// interface IStrategyOptionsWithRequest {
+//     usernameField: string;
+//     passwordField: string;
+//     passReqToCallback: true;
+// }
+// interface IVerifyOptions {
+//     message: string;
+// }
 
-interface VerifyFunctionWithRequest {
-    (req: express.Request, username: string, password: string, done: (error: any, user?: any, options?: IVerifyOptions) => void): void;
-}
+// interface VerifyFunctionWithRequest {
+//     (req: express.Request, username: string, password: string, done: (error: any, user?: any, options?: IVerifyOptions) => void): void;
+// }
 
-declare class LocalStrategy implements passport.Strategy {
-    constructor(options: IStrategyOptionsWithRequest, verify: VerifyFunctionWithRequest);
+// declare class LocalStrategy implements passport.Strategy {
+//     constructor(options: IStrategyOptionsWithRequest, verify: VerifyFunctionWithRequest);
 
-    name: string;
-    authenticate: (req: express.Request, options?: Object) => void;
-}
+//     name: string;
+//     authenticate: (req: express.Request, options?: Object) => void;
+// }
