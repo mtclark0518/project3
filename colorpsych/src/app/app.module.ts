f59f9bc8580bbba28b1e9fa208b8361735cd2107
mtclark0518 <mtclark0518@gmail.com>
2017-08-25 13:35:29 -0600

    this

--- colorpsych/src/app/app.module.ts
+++ colorpsych/src/app/app.module.ts
@@ -2,13 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
-<<<<<<< HEAD
-=======
 import { MaterializeModule }  from 'angular2-materialize';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
->>>>>>> aeb95dd1817e220ba51097173f38037f8a3dc7a9
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -28,10 +25,7 @@ import { AppRoutingModule } from './app.routing.module';
     NoopAnimationsModule,
     FormsModule,
     AppRoutingModule,
-<<<<<<< HEAD
-=======
     MaterializeModule.forRoot()
->>>>>>> aeb95dd1817e220ba51097173f38037f8a3dc7a9
   ],
   providers: [],
   bootstrap: [AppComponent]
