1bd0ceea3a022a33f4c50354ef5bc21196e78a94
mtclark0518 <mtclark0518@gmail.com>
2017-08-25 11:55:46 -0600

    creates app routing module and updates app.module.ts

--- colorpsych/src/app/app.module.ts
+++ colorpsych/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'colorpsych'}),
-    HttpModule
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
