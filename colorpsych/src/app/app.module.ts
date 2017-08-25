d0e8eb3f83f231c32c6a8e311f38cd1b5976044f
inordeck <ian.nordeck@gmail.com>
2017-08-25 07:25:03 -0600

    working on create by color, by color results, login, and signup components

--- colorpsych/src/app/app.module.ts
+++ colorpsych/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { CreateByColorComponent } from './src/app/create-by-color/create-by-color.component';
+import { ByColorResultComponent } from './src/app/by-color-result/by-color-result.component';
+import { LoginComponent } from './src/app/login/login.component';
+import { SignupComponent } from './src/app/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateByColorComponent,
+    ByColorResultComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'colorpsych'}),
---
+++ colorpsych/src/app/create-by-color/create-by-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-by-color',
+  templateUrl: './create-by-color.component.html',
+  styleUrls: ['./create-by-color.component.scss']
+})
+export class CreateByColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
