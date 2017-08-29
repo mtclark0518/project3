63e7c3b12bb628cf243ac810e5bb0153f5f99852
inordeck <ian.nordeck@gmail.com>
2017-08-29 11:26:10 -0600

    merge conflicts
    update broken paths to components

--- colorpsych/src/app/app.module.ts
+++ colorpsych/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { CreateByColorComponent } from './src/app/create-by-color/create-by-color.component';
-import { ByColorResultComponent } from './src/app/by-color-result/by-color-result.component';
-import { LoginComponent } from './src/app/login/login.component';
-import { SignupComponent } from './src/app/signup/signup.component';
+import { CreateByColorComponent } from './create-by-color/create-by-color.component';
+import { ByColorResultComponent } from './by-color-result/by-color-result.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
