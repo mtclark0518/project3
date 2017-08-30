29be7fc632763d264cd71ef5fd08470a0b1803ec
connie kephart <spindle.monkey@gmail.com>
2017-08-30 09:51:07 -0600

    adds first and last name to user model

--- colorpsych/src/models/user.js
+++ colorpsych/src/models/user.js
@@ -1,16 +1,20 @@
 module.exports = function(sequelize, Sequelize){
   var model = sequelize.define("user", {
-    // local: {
-      email: {
-        type: Sequelize.STRING,
-        notNull: true,
-        isEmail: true
-      },
-      password: {
-        type: Sequelize.STRING,
-        notNull: true
-      }
-    // },
+    email: {
+      type: Sequelize.STRING,
+      notNull: true,
+      isEmail: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      notNull: true
+    },
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    }
   });
   return model;
 };
